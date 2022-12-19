955108b72bdc7552f62e1320acc573bc0a996f9e
Noreen777 <noreenmalikinfo@gmail.com>
2022-12-19 21:19:09 +0400

    form-1

---
+++ cForm.php
@@ -0,0 +1,27 @@
+<?php
+
+$firstname = $_POST['First name'];
+$lastname = $_POST['Last name'];
+$visitor_email = $_POST['email'];
+$company_url = $_POST['Company url'];
+$message = $_POST['message'];
+
+$email_from = 'noreen-malik@outlook.com';
+$email_subject = "New querry submission";
+$email_body = "User First Name: $firstname.\n".
+"User Last Name: $lastname.\n".
+"User Email: $visitor_email.\n".
+"Company URL: $company_url.\n".
+"Message: $message.\n";
+
+$to = 'noreenmalikinfo@gmail.com';
+$headers = "From: $email_from \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
+
+mail($to, $email_subject, $email_body, $headers);
+header("Location: Hireme.html");
+
+
+
+
+?>
